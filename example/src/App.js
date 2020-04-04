a3f53429198d8115b1520bd0bfbce1ab8f5bfe97
suizyu <suizyu_m@yahoo.co.jp>
2020-04-05 01:23:47 +0900

    study props

--- example/src/App.js
+++ example/src/App.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
 
-// //クラスコンポｰネント
-// class ClassApp extends Component{
-
-//   constructor(){
-//   }
-
-//   render(){
-//     return <h1>render by class</h1>;
-//   }
-// }
-
-//関数コンポーネント
-const Ex = () => {
-  return <h1>Example</h1>
+//Ex props
+const Ex = (props) => {
+  return( 
+    <React.Fragment>
+      <h1>Hi, Here is {props.name}'s Page.</h1>
+      <p>you are {props.age} years old</p>
+    </React.Fragment>
+  )
 }
 
+Ex.defaultProps = {
+  age: 1
+};
+
 function App() {
+
+  const profile = [
+    {name: "Taro", age: 19},
+    {name: "Siro", age: 7},
+    {name: "John"},
+    {name: "Jiro", age: 15}
+  ]
+
   return (
     <React.Fragment>
-      <Ex />
-      <Ex />
-      <Ex />
+      {
+        profile.map((profile, index) =>  {
+          return <Ex name={profile.name} age={profile.age} key={index}/>
+        })
+      }
     </React.Fragment>
   );
 }
