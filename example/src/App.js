c300d72d810ef81f407513afc3f7eeb30cf144bb
suizyu <suizyu_m@yahoo.co.jp>
2020-04-05 00:29:36 +0900

    returnの書き方, JSXについて

--- example/src/App.js
+++ example/src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 function App() {
-  return (
-    <h1> Hello world!! </h1>
+  
+  const greeting = "Hi!!,"
+  const dom = <h1 className="foo">{greeting}  This is dom!!!</h1>
+
+  //属性名はキャメルケース
+
+  const inputLog =  <input type="text" onChange={() => {console.log("start input")}} />;
+
+  const forexample = (
+    <React.Fragment> 
+      <label htmlFor="bar">bar</label>
+      {inputLog}
+    </React.Fragment>
   );
+
+  //return で返すタグは1つでないといけない　→　要らない出力をなくす：React.Fragment
+
+  return forexample;
+
+  // return　の書き方
+  //return dom;
+  //return (dom);
+  
 }
 
 export default App;
