4f48936b3644661a7ac04444ff9a5c1f61bc5184
suizyu <suizyu_m@yahoo.co.jp>
2020-04-17 23:13:03 +0900

    誤字修正及び、App.jsをstateブランチから移植。不要ファイルの削除

--- example/src/components/App.js
+++ example/src/components/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World!
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Hello World!
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { Component } from 'react';
+
+// Propsは親のCompornentから値を渡される。
+// StateはCompornentの内部でのみ使用される
+// Popsは変更不能だが、Stateは変更可能
+
+const App = () => (<Counter></Counter>);
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)    
+    console.log(this.state)
+    this.state = { count: 0 }
+  }
+
+  countPlus = () => {
+    // NGパターン
+    // this.state = {count: this.state.count + 5};
+    // setStateが実行されると、コールバックでrenderが作動する。
+    
+    this.setState({ count: this.state.count + 1})
+  }
+
+  countMinus = () => {
+    this.setState({ count: this.state.count -1})
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={this.countPlus}>＋１</button>
+        <button onClick={this.countMinus}>－１</button>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- example/src/reducers/index.js
+++ example/src/reducers/index.js
@@ -1,7 +1,7 @@
-import { combineRducers } from 'reducx'
+import { combineReducers } from 'redux'
 import count from './count'
 
-export default combineRducers({ count })
+export default combineReducers({ count })
 
 //複数の場合
 //export default combineRducers({ foo, var, hoge })
