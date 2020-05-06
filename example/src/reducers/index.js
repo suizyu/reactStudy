1e1b89f7a5e12760cbca96ea563e0583c5603f13
suizyu <suizyu_m@yahoo.co.jp>
2020-05-06 22:10:50 +0900

    Add new SubmitEvent

--- example/src/reducers/index.js
+++ example/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 import events from './events'
 
-export default combineReducers({ events })
+export default combineReducers({ events, form })
 
 //複数の場合
 //export default combineRducers({ foo, var, hoge })
